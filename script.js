8c61d2730c133311d313c2251bb061ca427c26b1
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2023-08-04 18:32:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+document.title = 'My Portfolio';
+
+const skill = document.getElementById('skills');
+const project = document.getElementById('projects');
+const body = document.getElementsByTagName('body');
+
+
+// skill.style.backgroundColor = 'rgb(109, 74, 224)';
+// project.style.backgroundColor = '';
+
+// body.style.backgroundColor = 'rgb(109, 74, 224)';
